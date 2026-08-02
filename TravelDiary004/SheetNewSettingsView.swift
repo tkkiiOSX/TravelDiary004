dfc5bd2e46803150798cc96c25c1241e4395ac82
tkki_iOSX <tkki_iOSX@icloud.com>
2026-08-02 16:59:39 +0900

    新規シートの設定を追加したSheetNewSettingsViewに移動

---
+++ TravelDiary004/SheetNewSettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SheetNewSettingsView.swift
+//  旅行予定日記
+//
+//  Created by Xcode2021 on 2026/08/02.
+//
+
+import SwiftUI
+
+struct SheetNewSettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SheetNewSettingsView()
+}
